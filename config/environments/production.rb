efbc683737c53f3804a8c0350cd676fe3ec3dbdd
shimish000 <57582823+shimish000@users.noreply.github.com>
2020-05-12 19:31:22 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.initialize_on_precompile=false
+  
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -72,7 +73,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
